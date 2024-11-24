fc806bb45ffbd019192a1016089d669db726ea9c
LichtnerLevente <54024800+LichtnerLevente@users.noreply.github.com>
2024-11-24 21:00:11 +0100

    add update status to routes

--- diligent-academy-assessments/final/backend/src/application/actions/status/routes.ts
+++ diligent-academy-assessments/final/backend/src/application/actions/status/routes.ts
@@ -4,14 +4,16 @@ import { getAction } from "./get.action";
 import { createAction } from "./create.action";
 import { GetByBoardIdAction } from "./get-by-board_id.action";
 import { deleteAction } from "./delete.action";
+import { updateAction } from "./update.action";
 
 
 export default async function (fastify: FastifyInstance) {
     fastify.get("/boards/:id/statuses", GetByBoardIdAction);
+    // fastify.patch("/boards/:id/statuses/positions", updatePositionsAction);
     fastify.get("/statuses", getAllAction);
     fastify.post("/statuses", createAction);
     fastify.get("/statuses/:id", getAction);
-    // fastify.patch("/statuses/:id", );
+    fastify.patch("/statuses/:id", updateAction);
     fastify.delete("/statuses/:id/delete", deleteAction);
 
 }
